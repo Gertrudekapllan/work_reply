8d4c307a4028e48a7f8203c21b22e16b0e4491c7
Gertrudekapplan <easppolivanova@gmail.com>
2023-10-11 22:09:10 +0600

    all added

--- week6/lesson4-inheritance.py
+++ week6/lesson4-inheritance.py
@@ -34,10 +34,11 @@ class Dantist(Parent, Doctor):
 dastan = Dantist(3, "Dastan", 20, "male", 5)
 baha = Doctor("Baha", 27, "male")
 print(dastan.__dict__)
-# baha.health()
-# dastan.health()
-# dastan.earn(23456664)
-# print(dastan.wallet)
+baha.health()
+dastan.health()
+dastan.earn(23456664)
+print(dastan.wallet)
+
 
 class C():
     pass
@@ -52,6 +53,7 @@ class B(A):
     def test(self):
         return 3 + 7
 
+
 class D(B, C):
     def test(self):
         old_test = A.test(self)
@@ -60,3 +62,43 @@ class D(B, C):
 
 test_obj = D()
 test_obj.test()
+from functools import reduce
+
+# Создайте три класса: Person, Student и Teacher.
+# Класс Person должен содержать атрибуты name и age.
+# Класс Student должен наследовать от Person и добавить атрибут
+# student_id. Класс Teacher также должен наследовать от Person и
+# добавить атрибут employee_id. Создайте объекты для каждого класса и выведите их атрибуты.
+
+score_nazi = [("python", 243), ("python1", 213), ("python2", 223), ("python3", 233), ("python4", 423), ]
+
+
+class Person:
+    def __init__(self, name, age):
+        self.avg = None
+        self.name = name
+        self.age = age
+
+    def get_info(self):
+        print(self.name, self.age, self.avg)
+
+
+class Student(Person):
+
+    def __init__(self, name, age, student_id, score):
+        super(Student, self).__init__(name, age)
+        self.student_id = student_id
+        self.avg = reduce(lambda a, b: (None, a[1] + b[1]), score)[1] / len(score)
+
+
+class Teacher(Person):
+
+    def __init__(self, name, age, employee_id):
+        super().__init__(name, age)
+        self.employee_id = employee_id
+
+
+onj1 = Person("Test", 56)
+nazi = Student("Nazi", 27, 1232, score_nazi)
+nazi.get_info()
+onj1.get_info()
--- week6/lesson5-POLYMORPHISM.py
+++ week6/lesson5-POLYMORPHISM.py
@@ -1,36 +1,35 @@
-#ПОЛИМОРФИЗМ
-# полиморфизм
-# # ПОЛИМОРФИЗМ
-# # реализовать класс Student  который принимает в аргументы full_name, age, course
-# # также создать @classmethod который создает объект по full_name, year, admission(год поступления)
-# # и создать   @staticmethod который возвращает по admission(год поступления) курс
-# from datetime import date
-#
-# class Student:
-#
-#     def __init__(self, name, age, course):
-#         self.name = name
-#         self.age = age
-#         self.course = course
-#
-#     @classmethod
-#     def make_student_from_year(cls, name, year, course_year):
-#         my_age = 2023 - year
-#         course = cls.return_course(course_year)
-#         return cls(name, my_age, course)
-#
-#     @staticmethod
-#     def return_course(course_year):
-#         return 2023 - course_year
-#
-#     def info(self):
-#         print(self.age, self.name, self.course)
-#
-#
-# dastan = Student("Omurkanov Dastan", 22, 3)
-# baha = Student.make_student_from_year("Usenov Bahtyar", 2000, 2020)
-# dastan.info()
-# baha.info()
+
+# ПОЛИМОРФИЗМ
+# реализовать класс Student  который принимает в аргументы full_name, age, course
+# также создать @classmethod который создает объект по full_name, year, admission(год поступления)
+# и создать   @staticmethod который возвращает по admission(год поступления) курс
+from datetime import date
+
+class Student:
+
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+
+    @classmethod
+    def make_student_from_year(cls, name, year, course_year):
+        my_age = 2023 - year
+        course = cls.return_course(course_year)
+        return cls(name, my_age, course)
+
+    @staticmethod
+    def return_course(course_year):
+        return 2023 - course_year
+
+    def info(self):
+        print(self.age, self.name, self.course)
+
+
+dastan = Student("Omurkanov Dastan", 22, 3)
+baha = Student.make_student_from_year("Usenov Bahtyar", 2000, 2020)
+dastan.info()
+baha.info()
 
 # Создайте класс Employee,
 # представляющий сотрудника организации, с атрибутами,
@@ -74,9 +73,9 @@ class Employee:
 
 nazi = Employee("Nazgul", "Tabyldieva", 130000)
 anya = Employee.new_obj("Anya Polivanova 145000")
-# print(type(nazi.salary))
-# print(type(anya.salary))
-# print(Employee.employee_count)
+print(type(nazi.salary))
+print(type(anya.salary))
+print(Employee.employee_count)
 anya.procent(20)
 # +, *
 
@@ -84,82 +83,82 @@ anya.procent(20)
 # print("3" + "4")
 # print(["3", ] + ["4",])
 
-# class Dog:
-#
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def info(self):
-#         print("I am Dog, my name is {}".format(self.name))
-#
-#     def make_sound(self):
-#         print("gav gav")
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+
+    def info(self):
+        print("I am Dog, my name is {}".format(self.name))
+
+    def make_sound(self):
+        print("gav gav")
 
 
-# class Cat:
-#
-#     def __init__(self, name):
-#         self.name = name
-#
-#     def info(self):
-#         print("I am cat, my name is {}".format(self.name))
-#
-#     def make_sound(self):
-#         print("mew mew")
+class Cat:
+
+    def __init__(self, name):
+        self.name = name
+
+    def info(self):
+        print("I am cat, my name is {}".format(self.name))
+
+    def make_sound(self):
+        print("mew mew")
 
 
-# pitbul = Dog("Grisha")
+pitbul = Dog("Grisha")
 
-#
-# bengal = Cat("Test")
-# bengal1 = Cat("Test2")
-# object_list = [pitbul1, bengal, bengal1, pitbul]
-#
-# for i in object_list:
-#     i.info()
-#     i.make_sound()
-#
-# class Pitbul(Dog):
-#
-#     def make_sound(self):
-#         print("gav gav mrr mrr")
-#
-#
-# obj = Pitbul("Dog")
-# obj.info()
-# obj.make_sound()
-# pitbul1 = Dog("Grisha3")
-# pitbul1.make_sound()
+
+bengal = Cat("Test")
+bengal1 = Cat("Test2")
+object_list = [pitbul1, bengal, bengal1, pitbul]
+
+for i in object_list:
+    i.info()
+    i.make_sound()
+
+class Pitbul(Dog):
+
+    def make_sound(self):
+        print("gav gav mrr mrr")
+
+
+obj = Pitbul("Dog")
+obj.info()
+obj.make_sound()
+pitbul1 = Dog("Grisha3")
+pitbul1.make_sound()
 
 # staticmethod, classmethod, instance method
 
-# def area(r):
-#     import math
-#     return math.pi * r ** 2
+def area(r):
+    import math
+    return math.pi * r ** 2
 
 
-# class Pizza:
-#     def __init__(self, ingredients):
-#         self.ingredients = ingredients
-#
-#     def make_pizza(self):
-#
-#         print("Pizza consist of {0} area: {1}".format(self.ingredients, self.area(15)))
-#
-#     @classmethod
-#     def fabric_pizza(cls):
-#         print(cls.area(34))
-#         return cls(["ananas", "cheese mazarella", "cheese dorblu", "potato"])
-#
-#     @staticmethod
-#     def area(r):
-#         import math
-#         return math.pi * r**2
+class Pizza:
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
+
+    def make_pizza(self):
+
+        print("Pizza consist of {0} area: {1}".format(self.ingredients, self.area(15)))
+
+    @classmethod
+    def fabric_pizza(cls):
+        print(cls.area(34))
+        return cls(["ananas", "cheese mazarella", "cheese dorblu", "potato"])
+
+    @staticmethod
+    def area(r):
+        import math
+        return math.pi * r**2
 
 
-# obj = Pizza(["mozarella", "tomato", "cheese cheder"])
-# obj.make_pizza()
-# print(obj.area(15))
-# obj2 = Pizza.fabric_pizza()
-# obj2.make_pizza()
-# print(Pizza.area(25))
+obj = Pizza(["mozarella", "tomato", "cheese cheder"])
+obj.make_pizza()
+print(obj.area(15))
+obj2 = Pizza.fabric_pizza()
+obj2.make_pizza()
+print(Pizza.area(25))
